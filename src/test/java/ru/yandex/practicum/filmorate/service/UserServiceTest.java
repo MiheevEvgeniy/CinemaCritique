3d05d78dd6cb13f5d993c39937299ebafd697a40
zhenya93 <zhenya939393@yandex.ru>
2023-04-26 23:04:30 +0300

    add-database

--- src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
